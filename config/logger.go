bf0d874d7a9b2eb52407aeeca0561ed369fadf27
Danilo Faraum <rabelofaraumdanilo0123@gmail.com>
2024-10-05 18:05:07 -0300

    feat: improved logger

--- config/logger.go
+++ config/logger.go
@@ -6,6 +6,16 @@ import (
 	"os"
 )
 
+const (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Cyan   = "\033[36m"
+	Orange = "\033[38;5;208m"
+)
+
 type Logger struct {
 	debug   *log.Logger
 	info    *log.Logger
@@ -14,15 +24,19 @@ type Logger struct {
 	writer  io.Writer
 }
 
+func messageColor(prefix string, message string, color string) string {
+	return Orange + prefix + Reset + color + message + Reset
+}
+
 func NewLogger(prefix string) *Logger {
 	writer := os.Stdout                                    //Defines the writer as the deafault OS output
 	logger := log.New(writer, prefix, log.Ldate|log.Ltime) //Creates a logger with date and time in each log entry
 
 	return &Logger{
-		debug:   log.New(writer, "DEBUG: ", logger.Flags()),
-		info:    log.New(writer, "INFO: ", logger.Flags()),
-		warning: log.New(writer, "WARNING: ", logger.Flags()),
-		err:     log.New(writer, "ERROR: ", logger.Flags()),
+		debug:   log.New(writer, messageColor(prefix, " DEBUG: ", Blue), logger.Flags()),
+		info:    log.New(writer, messageColor(prefix, " INFO: ", Blue), logger.Flags()),
+		warning: log.New(writer, messageColor(prefix, " WARNING: ", Yellow), logger.Flags()),
+		err:     log.New(writer, messageColor(prefix, " ERROR: ", Red), logger.Flags()),
 		writer:  writer,
 	}
 }
