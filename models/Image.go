f04af92ddda60097028bcd6396102ef93e691b99
Danilo Faraum <rabelofaraumdanilo0123@gmail.com>
2024-10-07 16:50:53 -0300

    fix: correct atribute

--- models/Image.go
+++ models/Image.go
@@ -1,13 +1,13 @@
 package models
 
 type Image struct {
-	NameImage      string `gorm:"column:name_image;type:varchar(64)"`
+	Name           string `gorm:"column:name_image;type:varchar(64)"`
 	ID             int    `gorm:"primary_key;column:id;type:int;not null"`
 	EncryptedImage []byte `gorm:"column:encrypted_image;type:longblob;not null"`
 }
 
 type ImageResponse struct {
-	NameImage      string `json:"name_image"`
+	Name           string `json:"name"`
 	ID             int    `json:"id"`
 	EncryptedImage []byte `json:"encrypted_image"`
 }
