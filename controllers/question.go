6a97dbbf5f7cec4d49293d4ba07deea32429d2a0
Danilo Faraum <112991335+DaniloFaraum@users.noreply.github.com>
2024-10-26 22:52:37 +0000

    feat: add question crud

--- controllers/question.go
+++ controllers/question.go
@@ -1,1 +1,92 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/DaniloFaraum/studere-backend/models"
+	"github.com/DaniloFaraum/studere-backend/requests"
+	"github.com/DaniloFaraum/studere-backend/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateQuestionController(ctx *gin.Context) {
+    request := requests.CreateQuestionRequest{}
+
+    ctx.BindJSON(&request)
+
+    if err := request.Validate(); err != nil {
+        utils.HandleControllerError(ctx, http.StatusBadRequest, "request validation error", err)
+        return
+    }
+
+    question := models.Question{
+        //completar
+    }
+
+    if err := db.Create(&question).Error; err != nil {
+        utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not create question on db", err)
+        return
+    }
+
+    utils.SendSuccess(ctx, "create-question", request)
+}
+
+func ListQuestionsController(ctx *gin.Context) {
+	var questions []models.Question
+
+	if err := db.Find(&questions).Error; err!=nil{
+		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find questions", err)
+		return
+	}
+	utils.SendSuccess(ctx, "list-questions", questions)
+}
+
+func ShowQuestionController(ctx *gin.Context) {
+	id := ctx.Query("id")
+	if id == ""{
+		utils.SendError(ctx, http.StatusBadRequest, utils.ErrParamIsrequired("id","queryParam").Error())
+		return
+	}
+
+	question := models.Question{}
+
+	if err := db.First(&question, id).Error; err!=nil{
+		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find question", err)
+		return
+	}
+
+	utils.SendSuccess(ctx, "show-question", question)
+}
+
+func UpdateQuestionController(ctx *gin.Context) {
+	request := requests.UpdateQuestionRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+        utils.HandleControllerError(ctx, http.StatusBadRequest, "question validation error", err)
+		return
+	}
+
+	id := ctx.Query("id")
+	if id == ""{
+		utils.SendError(ctx, http.StatusBadRequest, utils.ErrParamIsrequired("id","queryParam").Error())
+		return
+	}
+
+	question := models.Question{}
+
+	if err := db.First(&question, id).Error; err!=nil{
+		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find question", err)
+		return
+	}
+
+	if request.Name != "" {
+        //uuuu
+    }
+
+	if err := db.Save(&question).Error; err!=nil{
+		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not update question", err)
+	}
+	utils.SendSuccess(ctx, "update-question", question)
+}
--- requests/question.go
+++ requests/question.go
@@ -1,30 +1,30 @@
 package requests
 
-// import (
-// 	"github.com/DaniloFaraum/studere-backend/utils"
-// )
+import (
+	"github.com/DaniloFaraum/studere-backend/utils"
+)
 
-// type CreateQuestionRequest struct {
-// 	Name            string `json:"name"`
-// 	IDQuestionnaire int    `json:"id_questionnaire"`
-// 	Question        string `json:"question"`
-// }
+type CreateQuestionRequest struct {
+	Name            string `json:"name"`
+	IDQuestionnaire int    `json:"id_questionnaire"`
+	Question        string `json:"question"`
+}
 
-// func (r *CreateQuestionRequest) Validate() error {
-// 	if r.Name == "" {
-// 		return utils.ErrParamIsrequired("name", "string")
-// 	}
-// 	return nil
-// }
+func (r *CreateQuestionRequest) Validate() error {
+	if r.Name == "" {
+		return utils.ErrParamIsrequired("name", "string")
+	}
+	return nil
+}
 
-// type UpdateQuestionRequest struct {
-// 	Name string `json:"name"`
-// }
+type UpdateQuestionRequest struct {
+	Name string `json:"name"`
+}
 
-// func (r *UpdateQuestionRequest) Validate() error {
-// 	if r.IDQuestionnaire != nil {
-// 		return nil
-// 	}
+func (r *UpdateQuestionRequest) Validate() error {
+	if r.IDQuestionnaire != nil {
+		return nil
+	}
 
-// 	return utils.ErrNoValidFields()
-// }
+	return utils.ErrNoValidFields()
+}
