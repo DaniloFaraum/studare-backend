36893afd7a2635dcd5e01efa3b3b2323425fb8d4
Danilo Faraum <rabelofaraumdanilo0123@gmail.com>
2024-11-16 19:38:41 -0300

    feat: new search methods

--- controllers/course.go
+++ controllers/course.go
@@ -1,148 +1,216 @@
 package controllers
 
 import (
-    "net/http"
+	"net/http"
+	"strconv"
 
-    "github.com/DaniloFaraum/studere-backend/models"
-    "github.com/DaniloFaraum/studere-backend/requests"
-    "github.com/DaniloFaraum/studere-backend/utils"
-    "github.com/gin-gonic/gin"
+	"github.com/DaniloFaraum/studere-backend/models"
+	"github.com/DaniloFaraum/studere-backend/requests"
+	"github.com/DaniloFaraum/studere-backend/utils"
+	"github.com/gin-gonic/gin"
 )
 
 func CreateCourseController(ctx *gin.Context) {
-    request := requests.CreateCourseRequest{}
+	request := requests.CreateCourseRequest{}
 
-    ctx.BindJSON(&request)
+	ctx.BindJSON(&request)
 
-    if err := request.Validate(); err != nil {
-        utils.HandleControllerError(ctx, http.StatusBadRequest, "course validation error", err)
-        return
-    }
+	if err := request.Validate(); err != nil {
+		utils.HandleControllerError(ctx, http.StatusBadRequest, "course validation error", err)
+		return
+	}
 
-    course := models.Course{
-		Name: request.Name,
+	course := models.Course{
+		Name:        request.Name,
 		Description: request.Description,
-		Link: request.Link,
-		Duration: request.Duration,
-		Author: request.Author,
+		Link:        request.Link,
+		Duration:    request.Duration,
+		Author:      request.Author,
 		Institution: request.Institution,
-		IDImage: request.IDImage,
-    }
+		IDImage:     request.IDImage,
+	}
 
-    if err := db.Create(&course).Error; err != nil {
-        utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not create course on db", err)
-        return
-    }
+	if err := db.Create(&course).Error; err != nil {
+		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not create course on db", err)
+		return
+	}
 
-    utils.SendSuccess(ctx, "create-course", request)
+	utils.SendSuccess(ctx, "create-course", request)
 }
 
 func ListCoursesController(ctx *gin.Context) {
-    var courses []models.Course
-    var courseResponses []models.CourseResponse
+	var courses []models.Course
+	var courseResponses []models.CourseResponse
 
-    if err := db.Find(&courses).Error; err != nil {
-        utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find courses", err)
-        return
-    }
+	if err := db.Find(&courses).Error; err != nil {
+		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find courses", err)
+		return
+	}
 
-    for _, course := range courses {
-        courseResponses = append(courseResponses, models.CourseResponse{
-			ID: course.ID,
-			Name: course.Name,
+	for _, course := range courses {
+		courseResponses = append(courseResponses, models.CourseResponse{
+			ID:          course.ID,
+			Name:        course.Name,
 			Description: course.Description,
-			Link: course.Link,
-			Rating: course.Rating,
-			Duration: course.Duration,
-			Author: course.Author,
+			Link:        course.Link,
+			Rating:      course.Rating,
+			Duration:    course.Duration,
+			Author:      course.Author,
 			Institution: course.Institution,
-			IDImage: course.IDImage,
-        })
-    }
+			IDImage:     course.IDImage,
+		})
+	}
 
-    utils.SendSuccess(ctx, "list-courses", courseResponses)
+	utils.SendSuccess(ctx, "list-courses", courseResponses)
 }
 
 func ShowCourseController(ctx *gin.Context) {
-    id := ctx.Param("id")
-    if id == "" {
-        utils.SendError(ctx, http.StatusBadRequest, utils.ErrParamIsrequired("id", "queryParam").Error())
-        return
-    }
-
-    course := models.Course{}
-
-    if err := db.First(&course, id).Error; err != nil {
-        utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find course", err)
-        return
-    }
-
-    utils.SendSuccess(ctx, "show-course", course)
-}
-
-func UpdateCourseController(ctx *gin.Context) {
-    request := requests.UpdateCourseRequest{}
-
-    ctx.BindJSON(&request)
-
-    if err := request.Validate(); err != nil {
-        utils.HandleControllerError(ctx, http.StatusBadRequest, "course validation error", err)
-        return
-    }
-
-    id := ctx.Param("id")
-    if id == "" {
-        utils.SendError(ctx, http.StatusBadRequest, utils.ErrParamIsrequired("id", "queryParam").Error())
-        return
-    }
-
-    course := models.Course{}
-
-    if err := db.First(&course, id).Error; err != nil {
-        utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find course", err)
-        return
-    }
-
-    // Update the fields based on the request
-    switch {
-    case request.Name != "":
-        course.Name = request.Name
-    case request.Description != "":
-        course.Description = request.Description
-    case request.Link != "":
-        course.Link = request.Link
-    case !request.Duration.IsZero() :
-        course.Duration = request.Duration
-    case request.Author != "":
-        course.Author = request.Author
-    case request.Institution != "":
-        course.Institution = request.Institution
-    }
-
-    if err := db.Save(&course).Error; err != nil {
-        utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not update course", err)
-    }
-    utils.SendSuccess(ctx, "update-course", course)
-}
-
-func DeleteCourseController(ctx *gin.Context) {
 	id := ctx.Param("id")
-	if id == ""{
-		utils.SendError(ctx, http.StatusBadRequest, utils.ErrParamIsrequired("id","queryParam").Error())
+	if id == "" {
+		utils.SendError(ctx, http.StatusBadRequest, utils.ErrParamIsrequired("id", "queryParam").Error())
 		return
 	}
 
 	course := models.Course{}
 
-	if err := db.First(&course, id).Error; err!=nil{
+	if err := db.First(&course, id).Error; err != nil {
 		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find course", err)
 		return
 	}
 
-	if err := db.Delete(&course).Error; err!=nil{
+	utils.SendSuccess(ctx, "show-course", course)
+}
+
+func UpdateCourseController(ctx *gin.Context) {
+	request := requests.UpdateCourseRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		utils.HandleControllerError(ctx, http.StatusBadRequest, "course validation error", err)
+		return
+	}
+
+	id := ctx.Param("id")
+	if id == "" {
+		utils.SendError(ctx, http.StatusBadRequest, utils.ErrParamIsrequired("id", "queryParam").Error())
+		return
+	}
+
+	course := models.Course{}
+
+	if err := db.First(&course, id).Error; err != nil {
+		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find course", err)
+		return
+	}
+
+	// Update the fields based on the request
+	switch {
+	case request.Name != "":
+		course.Name = request.Name
+	case request.Description != "":
+		course.Description = request.Description
+	case request.Link != "":
+		course.Link = request.Link
+	case !request.Duration.IsZero():
+		course.Duration = request.Duration
+	case request.Author != "":
+		course.Author = request.Author
+	case request.Institution != "":
+		course.Institution = request.Institution
+	}
+
+	if err := db.Save(&course).Error; err != nil {
+		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not update course", err)
+	}
+	utils.SendSuccess(ctx, "update-course", course)
+}
+
+func DeleteCourseController(ctx *gin.Context) {
+	id := ctx.Param("id")
+	if id == "" {
+		utils.SendError(ctx, http.StatusBadRequest, utils.ErrParamIsrequired("id", "queryParam").Error())
+		return
+	}
+
+	course := models.Course{}
+
+	if err := db.First(&course, id).Error; err != nil {
+		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find course", err)
+		return
+	}
+
+	if err := db.Delete(&course).Error; err != nil {
 		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not delete course", err)
 		return
 	}
 
 	utils.SendSuccess(ctx, "delete-course", course)
+}
+
+func SearchByName(ctx *gin.Context) {
+	name := ctx.Query("name")
+	ratingStr := ctx.Query("rating")
+	durationStr := ctx.Query("duration")
+	author := ctx.Query("author")
+
+	var rating, duration float64
+	var err error
+
+	if ratingStr != "" {
+		rating, err = strconv.ParseFloat(ratingStr, 64)
+		if err != nil {
+			utils.SendError(ctx, http.StatusBadRequest, "invalid rating")
+			return
+		}
+	}
+
+	if durationStr != "" {
+		duration, err = strconv.ParseFloat(durationStr, 64)
+		if err != nil {
+			utils.SendError(ctx, http.StatusBadRequest, "invalid duration")
+			return
+		}
+	}
+
+	var courses []models.Course
+	query := db.Model(&models.Course{})
+
+	if name != "" {
+		query = query.Where("name LIKE ?", "%"+name+"%")
+	}
+	if ratingStr != "" {
+		query = query.Where("rating >= ?", rating)
+	}
+	if durationStr != "" {
+		query = query.Where("duration <= ?", duration)
+	}
+	if author != "" {
+		query = query.Where("author LIKE ?", "%"+author+"%")
+	}
+
+	if err := query.Find(&courses).Error; err != nil {
+		utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find courses", err)
+		return
+	}
+
+	utils.SendSuccess(ctx, "search-courses", courses)
+}
+
+func SearchCoursesByTagsController(ctx *gin.Context) {
+    tags := ctx.QueryArray("tags")
+
+    var courses []models.Course
+    query := db.Model(&models.Course{}).Joins("JOIN course_tags ON course_tags.course_id = courses.id").Joins("JOIN tags ON tags.id = course_tags.tag_id")
+
+    if len(tags) > 0 {
+        query = query.Where("tags.name IN ?", tags)
+    }
+
+    if err := query.Find(&courses).Error; err != nil {
+        utils.HandleControllerError(ctx, http.StatusInternalServerError, "could not find courses", err)
+        return
+    }
+
+    utils.SendSuccess(ctx, "search-courses-by-tags", courses)
 }
